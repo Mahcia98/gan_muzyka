3253b59ac0f711532be33ac4dd76ce839ad7b9f7
mateuszdorobek <mateuszdorobek@gmail.com>
2023-05-15 17:08:20 +0200

    add constants

---
+++ constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+MAESTRO_DATA_URL = "https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip"
+
+DATA_DIR_PATH = Path('data')
+DATA_DIR_PATH.mkdir(exist_ok=True)
+
+MAESTR_ZIP_FILE_PATH = DATA_DIR_PATH / "maestro-v3.0.0-midi.zip"
+MAESTRO_DATASET_PATH = DATA_DIR_PATH / 'maestro-v3.0.0'
+SPARSE_ARRAY_DATASET_FILE = DATA_DIR_PATH / 'training_dataset.npz'
