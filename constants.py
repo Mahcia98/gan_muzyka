2601c4aaa8f97abc99e551b4f7e2715660d2c512
mateuszdorobek <mateuszdorobek@gmail.com>
2023-10-18 15:56:49 +0200

    add midi generation

--- constants.py
+++ constants.py
@@ -8,3 +8,6 @@ DATA_DIR_PATH.mkdir(exist_ok=True)
 MAESTR_ZIP_FILE_PATH = DATA_DIR_PATH / "maestro-v3.0.0-midi.zip"
 MAESTRO_DATASET_PATH = DATA_DIR_PATH / 'maestro-v3.0.0'
 SPARSE_ARRAY_DATASET_FILE = DATA_DIR_PATH / 'training_dataset.npz'
+
+MIDI_PATH = Path('midi')
+MIDI_PATH.mkdir(exist_ok=True)
--- generate_music.py
+++ generate_music.py
@@ -1,11 +1,52 @@
 import click
+from mido import Message, MidiFile, MidiTrack
 
-from constants import SPARSE_ARRAY_DATASET_FILE
+from constants import SPARSE_ARRAY_DATASET_FILE, MIDI_PATH
 from data_utils import SparseDataLoader
 from train_model import MODELS_PATH, GAN
 
 
-def generate(load_from_path, n_samples=1):
+def roll_to_track(roll):
+    def stop_note(note, time):
+        return Message('note_off', note=note, velocity=0, time=time)
+
+    def start_note(note, time):
+        return Message('note_on', note=note, velocity=127, time=time)
+
+    delta = 0
+    # State of the notes in the roll.
+    notes = [False] * len(roll[0])
+    # MIDI note for first column.
+    midi_base = 0
+    for row in roll:
+        for i, col in enumerate(row):
+            note = midi_base + i
+            if col == 1:
+                if notes[i]:
+                    # First stop the ringing note
+                    yield stop_note(note, delta)
+                    delta = 0
+                yield start_note(note, delta)
+                delta = 0
+                notes[i] = True
+            elif col == 0:
+                if notes[i]:
+                    # Stop the ringing note
+                    yield stop_note(note, delta)
+                    delta = 0
+                notes[i] = False
+        # ms per row
+        delta += 500
+
+
+def generate_midi_from_array(arr, name='sample'):
+    midi = MidiFile(type=1)
+    midi.tracks.append(MidiTrack(roll_to_track(arr.T)))
+    midi_file_path = MIDI_PATH / f'{name}.mid'
+    midi.save(midi_file_path)
+
+
+def generate(load_from_path, name):
     print("Creating SparseDataLoader")
     data_loader = SparseDataLoader(
         file_name=SPARSE_ARRAY_DATASET_FILE,  # File name of the dataset
@@ -22,19 +63,21 @@ def generate(load_from_path, n_samples=1):
         print("Loading Model")
         gan.load_model(load_from_path)
     # Generate Music Samples
-    X_fake, _ = gan.get_fake_images(n_samples=n_samples)
-    print(X_fake.shape)
+    X_fake, _ = gan.get_fake_images(n_samples=1)
+    arr = X_fake[0].reshape(X_fake.shape[1], X_fake.shape[2]).astype(int)
+    print(arr.shape)
+    generate_midi_from_array(arr=arr, name=name)
 
 
 @click.command()
-@click.option('--n_samples', type=int, default=1, help='Number of sample (integer)')
 @click.option('--load_from_path', type=str, help='Path to load from (string)')
-def cli(n_samples, load_from_path):
+@click.option('--name', type=str, help='name of output file (string)')
+def cli(name, load_from_path):
     """
     Usage:
-        python generate_music.py --n_samples 1 --load_from_path 'gan_save_2023_10_18'
+        python generate_music.py --load_from_path 'gan_save_2023_10_18' --name 'sample'
     """
-    generate(n_samples=n_samples, load_from_path=MODELS_PATH/load_from_path)
+    generate(name=name, load_from_path=MODELS_PATH / load_from_path)
 
 
 if __name__ == "__main__":
